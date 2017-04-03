d826100172912781246008626b7d45824e02d632
Fafa13 <fabrice.lopez@inserm.fr>
2017-04-03 19:25:21 +0200

    added the possibility to write results in a file with print_gtf_data function

--- libgtftk.c
+++ libgtftk.c
@@ -238,9 +238,17 @@ int add_row_list(ROW_LIST *src, ROW_LIST *dst) {
  * 		gtf_data:	a pointer on the GTF data to be printed
  */
 __attribute__ ((visibility ("default")))
-void print_gtf_data(GTF_DATA *gtf_data, FILE *output) {
+void print_gtf_data(GTF_DATA *gtf_data, char *output) {
 	int i;
-	for (i = 0; i < gtf_data->size; i++) print_row(output, &gtf_data->data[i], '\t');
+	FILE *out = stdout;
+
+	if (*output != '-') out = fopen(output, "w");
+	if (out == NULL) out = stdout;
+	for (i = 0; i < gtf_data->size; i++) print_row(out, &gtf_data->data[i], '\t');
+	if (out != stdout) {
+		fflush(out);
+		fclose(out);
+	}
 }
 
 /*
