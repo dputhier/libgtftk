4fed391fd3bb5fe8d3bf23ff3c9d2a23805f9eff
Fafa13 <fabrice.lopez@inserm.fr>
2017-05-09 16:28:28 +0200

    test null pointer in free_gtf_data

--- command/free_gtf_data.c
+++ command/free_gtf_data.c
@@ -13,19 +13,22 @@ int free_gtf_data(GTF_DATA *gtf_data) {
 	GTF_ROW *row;
 
 	//fprintf(stderr, "%d\n", gtf_data->size);
-	for (i = 0; i < gtf_data->size; i++) {
-		row = &gtf_data->data[i];
-		for (j = 0; j < 8; j++)	free(row->field[j]);
-		free(row->field);
+	if (gtf_data != NULL) {
+		for (i = 0; i < gtf_data->size; i++) {
+			row = &gtf_data->data[i];
+			for (j = 0; j < 8; j++)	free(row->field[j]);
+			free(row->field);
 
-		for (j = 0; j < row->nb_attributes; j++) {
-			free(row->key[j]);
-			free(row->value[j]);
+			for (j = 0; j < row->nb_attributes; j++) {
+				free(row->key[j]);
+				free(row->value[j]);
+			}
+			free(row->key);
+			free(row->value);
 		}
-		free(row->key);
-		free(row->value);
+		free(gtf_data->data);
+		free(gtf_data);
+		gtf_data = NULL;
 	}
-	free(gtf_data->data);
-	free(gtf_data);
 	return 0;
 }
