57d5601e61118412976fc989bdcdd2aa6cb9a48e
Fafa13 <fabrice.lopez@inserm.fr>
2017-06-08 13:02:03 +0200

    first commit in branch 3.2

---
+++ command/del_attributes.c
@@ -0,0 +1,42 @@
+/*
+ * del_attributes.c
+ *
+ *  Created on: Jun 8, 2017
+ *      Author: fafa
+ */
+
+#include "libgtftk.h"
+
+/*
+ * external functions declaration
+ */
+extern GTF_DATA *clone(GTF_DATA *gtf_data);
+
+__attribute__ ((visibility ("default")))
+GTF_DATA *del_attributes(GTF_DATA *gtf_data, char *features, char *keys) {
+	int i, ok, j;
+
+	/*
+	 * reserve memory for the GTF_DATA structure to return
+	 */
+	GTF_DATA *ret = clone(gtf_data);
+
+	GTF_ROW *row;
+
+	for (i = 0; i < ret->size; i++) {
+		row = ret->data[i];
+		ok = (features == NULL);
+		if (!ok) ok = strstr(features, row->field[2]);
+		if (ok) {
+			for (j = 0; j < row->nb_attributes; j++) {
+				if (strstr(keys, row->key[j])) {
+					free(row->key[j]);
+					free(row->value[j]);
+
+				}
+			}
+		}
+	}
+
+	return ret;
+}
--- libgtftk.h
+++ libgtftk.h
@@ -314,5 +314,6 @@ TTEXT *get_attribute_list(GTF_DATA *gtf_data);
 TTEXT *get_attribute_values_list(GTF_DATA *gtf_data, char *attribute);
 GTF_DATA *convert_to_ensembl(GTF_DATA *gtf_data);
 GTF_DATA *add_attributes(GTF_DATA *gtf_data, char *features, char *key, char *new_key, char *inputfile_name);
+GTF_DATA *del_attributes(GTF_DATA *gtf_data, char *features, char *keys);
 
 #endif /* GTFTOOLKIT_GTFTK_SRC_LIB_LIBGTFTK_H_ */
