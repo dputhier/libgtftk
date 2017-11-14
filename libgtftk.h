d78cbdc49301b84b9e38e831e8f32d5d56959e6c
puthier <puthier@gmail.com>
2017-11-14 17:08:35 +0100

    The ";" malediction.

--- libgtftk.h
+++ libgtftk.h
@@ -354,7 +354,7 @@ GTF_DATA *add_attributes(GTF_DATA *gtf_data, char *features, char *key, char *ne
 GTF_DATA *del_attributes(GTF_DATA *gtf_data, char *features, char *keys);
 GTF_DATA *select_by_positions(GTF_DATA *gtf_data, int *pos, int size);
 GTF_DATA *add_exon_number(GTF_DATA *gtf_data, char *exon_number_field);
-GTF_DATA *add_prefix(GTF_DATA *gtf_data, char *features, char *key, char *txt, int suffix)
+GTF_DATA *add_prefix(GTF_DATA *gtf_data, char *features, char *key, char *txt, int suffix);
 
 
 #endif /* GTFTOOLKIT_GTFTK_SRC_LIB_LIBGTFTK_H_ */
